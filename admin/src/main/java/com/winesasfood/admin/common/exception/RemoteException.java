e7190088bdff567839cff47a77d6cebc2b6357b3
周唯奇 <2796065735@qq.com>
2026-01-16 15:18:18 +0800

    开发异常处理相关的代码

---
+++ admin/src/main/java/com/winesasfood/admin/common/exception/RemoteException.java
@@ -0,0 +1,27 @@
+package com.winesasfood.admin.common.exception;
+
+import com.winesasfood.admin.common.errorcode.BaseErrorCode;
+import com.winesasfood.admin.common.errorcode.IErrorCode;
+
+public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
+
+    public RemoteException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public RemoteException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "RemoteException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
