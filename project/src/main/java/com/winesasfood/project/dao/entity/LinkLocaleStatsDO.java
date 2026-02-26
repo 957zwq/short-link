b5b2254166a040526493cc235161739805ee76c7
周唯奇 <2796065735@qq.com>
2026-02-26 20:32:20 +0800

    feature: 开发短链接监控之地区访问数据统计

---
+++ project/src/main/java/com/winesasfood/project/dao/entity/LinkLocaleStatsDO.java
@@ -0,0 +1,61 @@
+package com.winesasfood.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.winesasfood.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 短链接地区访问统计 DO
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_link_locale_stats")
+public class LinkLocaleStatsDO extends BaseDO {
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 国家
+     */
+    private String country;
+
+    /**
+     * 省份
+     */
+    private String province;
+
+    /**
+     * 城市
+     */
+    private String city;
+
+    /**
+     * 城市编码
+     */
+    private String adcode;
+}
